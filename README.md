604e7f53853c7494bbc0507561f990ad39bed8a9
haijinboys <haijin.boys@gmail.com>
2025-08-20 22:21:49 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# win11dwmfix
-This DLL prevents the afterimage that appears momentarily in the non-client area when you open a Win32 application window in a Windows 11 environment.
+# Win11DwmFix
+
+Windows 11 環境において、Win32 アプリケーションのウィンドウを開いた際に発生する、非クライアント領域の残像が一瞬表示される現象を回避するための DLL です。
+
+## 特徴
+
+- DLL を LoadLibrary するだけで、アプリに簡単に適用できます。
+- Mery のプラグインとしても使用できます。
+
+## 環境
+
+**動作環境**
+- Windows 11
+
+**ビルド環境**
+- Delphi XE2 .. 12
+
+## 使い方
+
+**Delphi**
+```delphi
+var
+  FModule: HMODULE;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  FModule := LoadLibrary('Win11DwmFix.dll');
+end;
+```
+
+**C++**
+```cpp
+HMODULE hModule = LoadLibrary(L"Win11DwmFix.dll");
+```
+
+**Mery のプラグインとして使用する場合**
+1. アーカイブを展開し、`Win11DwmFix.dll` を `Mery.exe` のインストール先の `Plugins` フォルダーにコピーしてください。
+2. Mery を起動すると、自動的に適用されます。
+
+## 注意事項
+
+- 本 DLL により、当該現象が必ずしも解消されることを保証するものではありません。
+- 将来的に Microsoft により当該現象が修正される可能性があります。その場合は、本 DLL を速やかに削除してください。
---
+++ Win11DwmFix.dpr
@@ -0,0 +1,227 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) Kuro. All rights reserved.
+// Licensed under the MIT License.
+// -----------------------------------------------------------------------------
+
+library Win11DwmFix;
+
+{$R 'Resource.res' 'Resource.rc'}
+
+{$IFDEF DEBUG}
+{$R *.res}
+{$ENDIF}
+
+
+uses
+  Winapi.Windows,
+  Winapi.Messages;
+
+resourcestring
+  SName = 'Win11ShowWindowFix';
+  SVersion = '1.0.0';
+
+const
+  IDS_TEXT = 1;
+
+const
+  MP_FIRST = WM_USER + $0500;
+  MP_GET_NAME = MP_FIRST + 2;
+  MP_GET_VERSION = MP_FIRST + 3;
+
+const
+  DWMWA_ALLOW_NCPAINT = 4;
+
+type
+  TCallWndHook = record
+    ThreadId: Cardinal;
+    Hook: HHOOK;
+  end;
+
+var
+  SaveDllProc: TDLLProc;
+  FNcPaint: array of HWND;
+  FCallWndHooks: array of TCallWndHook;
+
+function DwmSetWindowAttribute(hwnd: HWND; dwAttribute: DWORD;
+  pvAttribute: Pointer; cbAttribute: DWORD): HResult; stdcall;
+  external 'DWMAPI.DLL' name 'DwmSetWindowAttribute' delayed;
+
+function IndexOfHWND(hwnd: HWND): Integer;
+var
+  I: Integer;
+begin
+  for i := 0 to Length(FNcPaint) - 1 do
+    if FNcPaint[I] = hwnd then
+      Exit(I);
+  Result := -1;
+end;
+
+function IndexOfThread(AThreadId: Cardinal): Integer;
+var
+  I: Integer;
+begin
+  for I := 0 to Length(FCallWndHooks) - 1 do
+    if FCallWndHooks[I].ThreadId = AThreadId then
+      Exit(I);
+  Result := -1;
+end;
+
+function CallWindowHook(Code: Integer; wParam: WPARAM;
+  Msg: PCWPStruct): LongInt; stdcall;
+var
+  I, LIndex: Integer;
+  LAttribute: BOOL;
+begin
+  if Code = HC_ACTION then
+    case Msg.message of
+      WM_SHOWWINDOW:
+        if (Msg.wParam <> 0) and ((GetWindowLong(Msg.hwnd, GWL_STYLE) and WS_CHILD) = 0) then
+        begin
+          if IndexOfHWND(Msg.hwnd) < 0 then
+          begin
+            LAttribute := True;
+            DwmSetWindowAttribute(Msg.hwnd, DWMWA_ALLOW_NCPAINT, @LAttribute, SizeOf(LAttribute));
+            SetLength(FNcPaint, Length(FNcPaint) + 1);
+            FNcPaint[Length(FNcPaint) - 1] := Msg.hwnd;
+          end;
+        end;
+      WM_NCPAINT:
+        begin
+          LIndex := IndexOfHWND(Msg.hwnd);
+          if LIndex > -1 then
+          begin
+            LAttribute := False;
+            DwmSetWindowAttribute(Msg.hwnd, DWMWA_ALLOW_NCPAINT, @LAttribute, SizeOf(LAttribute));
+            for I := LIndex to Length(FNcPaint) - 2 do
+              FNcPaint[I] := FNcPaint[I + 1];
+            SetLength(FNcPaint, Length(FNcPaint) - 1);
+          end;
+        end;
+    end;
+  LIndex := IndexOfThread(GetCurrentThreadId);
+  if LIndex > -1 then
+    Result := CallNextHookEx(FCallWndHooks[LIndex].Hook, Code, wParam, LPARAM(Msg))
+  else
+    Result := 0;
+end;
+
+procedure DllEntry(Reason: Integer);
+var
+  I, LIndex: Integer;
+  LThreadId: Cardinal;
+begin
+  LThreadId := GetCurrentThreadId;
+  case Reason of
+    DLL_PROCESS_ATTACH:
+      begin
+        SetLength(FNcPaint, 0);
+        SetLength(FCallWndHooks, 0);
+      end;
+    DLL_THREAD_ATTACH:
+      if IndexOfThread(LThreadId) < 0 then
+      begin
+        SetLength(FCallWndHooks, Length(FCallWndHooks) + 1);
+        with FCallWndHooks[Length(FCallWndHooks) - 1] do
+        begin
+          ThreadId := LThreadId;
+          Hook := SetWindowsHookEx(WH_CALLWNDPROC, @CallWindowHook, 0, LThreadId);
+        end;
+      end;
+    DLL_THREAD_DETACH:
+      begin
+        LIndex := IndexOfThread(LThreadId);
+        if LIndex > -1 then
+        begin
+          UnhookWindowsHookEx(FCallWndHooks[LIndex].Hook);
+          for I := LIndex to Length(FCallWndHooks) - 2 do
+            FCallWndHooks[I] := FCallWndHooks[I + 1];
+          SetLength(FCallWndHooks, Length(FCallWndHooks) - 1);
+        end;
+      end;
+    DLL_PROCESS_DETACH:
+      begin
+        for I := 0 to Length(FCallWndHooks) - 1 do
+          UnhookWindowsHookEx(FCallWndHooks[I].Hook);
+        SetLength(FCallWndHooks, 0);
+        SetLength(FNcPaint, 0);
+      end;
+  end;
+  if Assigned(SaveDllProc) then
+    SaveDllProc(Reason);
+end;
+
+procedure OnCommand(hwnd: HWND); stdcall;
+begin
+  //
+end;
+
+function QueryStatus(hwnd: HWND; pbChecked: PBOOL): BOOL; stdcall;
+begin
+  pbChecked^ := True;
+  Result := True;
+end;
+
+function GetMenuTextID: Cardinal; stdcall;
+begin
+  Result := IDS_TEXT;
+end;
+
+function GetStatusMessageID: Cardinal; stdcall;
+begin
+  Result := IDS_TEXT;
+end;
+
+function GetIconID: Cardinal; stdcall;
+begin
+  Result := 0;
+end;
+
+function GetIconDarkID: Cardinal; stdcall;
+begin
+  Result := 0;
+end;
+
+procedure OnEvents(hwnd: HWND; nEvent: Cardinal; lParam: LPARAM); stdcall;
+begin
+  //
+end;
+
+function PluginProc(hwnd: HWND; nMsg: Cardinal; wParam: WPARAM; lParam: LPARAM): LRESULT; stdcall;
+begin
+  Result := 0;
+  case nMsg of
+    MP_GET_NAME:
+      begin
+        Result := Length(SName);
+        if lParam <> 0 then
+          lstrcpynW(PChar(lParam), PChar(SName), wParam);
+      end;
+    MP_GET_VERSION:
+      begin
+        Result := Length(SVersion);
+        if lParam <> 0 then
+          lstrcpynW(PChar(lParam), PChar(SVersion), wParam);
+      end;
+  end;
+end;
+
+exports
+  OnCommand,
+  QueryStatus,
+  GetMenuTextID,
+  GetStatusMessageID,
+  GetIconID,
+  GetIconDarkID,
+  OnEvents,
+  PluginProc;
+
+begin
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+{$ENDIF}
+  SaveDllProc := @DllProc;
+  DllProc := @DllEntry;
+  DllEntry(DLL_PROCESS_ATTACH);
+  DllEntry(DLL_THREAD_ATTACH);
+
+end.
